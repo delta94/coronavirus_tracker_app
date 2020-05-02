388cf277022d3aac88696419efa645012207f639
Vinoth Vino <vinothvino42@gmail.com>
2020-05-02 08:12:59 +0530

    Add get endpoint data functions and modify rest api http file

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,6 +1,14 @@
 import 'package:coronavirus_tracker_app/app/services/api_keys.dart';
 import 'package:flutter/foundation.dart';
 
+enum Endpoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
 class Api {
   final String apiKey;
 
@@ -9,6 +17,7 @@ class Api {
   factory Api.sandbox() => Api(apiKey: ApiKeys.ncovSandboxKey);
 
   static final String host = 'apigw.nubentos.com';
+  static final String basePath = 't/nubentos.com/ncovapi/1.0.0';
   static final int port = 443;
 
   Uri tokenUri() => Uri(
@@ -18,4 +27,19 @@ class Api {
         path: 'token',
         queryParameters: {'grant_type': 'client_credentials'},
       );
+
+  Uri endpointUri(Endpoint endPoint) => Uri(
+        scheme: 'https',
+        host: host,
+        port: port,
+        path: '$basePath/${_paths[endPoint]}',
+      );
+
+  static Map<Endpoint, String> _paths = {
+    Endpoint.casesSuspected: 'cases/suspected',
+    Endpoint.casesConfirmed: 'cases/confirmed',
+    Endpoint.deaths: 'deaths',
+    Endpoint.recovered: 'recovered',
+    Endpoint.cases: 'cases',
+  };
 }
