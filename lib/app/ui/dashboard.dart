80cf44d033925558d2e735290e93b10184f8aef5
Vinoth Vino <vinothvino42@gmail.com>
2020-05-03 18:25:21 +0530

    Add platform alert dialog

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:coronavirus_tracker_app/app/repositories/data_repository.dart';
 import 'package:coronavirus_tracker_app/app/repositories/endpoints_data.dart';
 import 'package:coronavirus_tracker_app/app/services/api.dart';
 import 'package:coronavirus_tracker_app/app/ui/endpoint_card.dart';
 import 'package:coronavirus_tracker_app/app/ui/last_updated_status_text.dart';
+import 'package:coronavirus_tracker_app/app/ui/show_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,9 +18,19 @@ class _DashboardState extends State<Dashboard> {
   EndpointsData _endpointsData;
 
   Future<void> _updateData() async {
-    final dataRepository = Provider.of<DataRepository>(context, listen: false);
-    final endpointData = await dataRepository.getAllEndpointsData();
-    setState(() => _endpointsData = endpointData);
+    try {
+      final dataRepository =
+          Provider.of<DataRepository>(context, listen: false);
+      final endpointData = await dataRepository.getAllEndpointsData();
+      setState(() => _endpointsData = endpointData);
+    } on SocketException catch (_) {
+      showAlertDialog(
+        context: context,
+        title: 'Connection Error',
+        content: 'Could not retrieve data, please try again later',
+        defaultActionText: 'Ok',
+      );
+    }
   }
 
   @override
@@ -28,7 +41,11 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
-    final formatter = LastUpdatedDateFormatter(lastUpdated: _endpointsData != null ? _endpointsData.values[Endpoint.cases].date : null,);
+    final formatter = LastUpdatedDateFormatter(
+      lastUpdated: _endpointsData != null
+          ? _endpointsData.values[Endpoint.cases].date
+          : null,
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text('Coronavirus Tracker'),
@@ -41,7 +58,9 @@ class _DashboardState extends State<Dashboard> {
             for (var endpoint in Endpoint.values)
               EndpointCard(
                 endpoint: endpoint,
-                value: _endpointsData != null ? _endpointsData.values[endpoint].value : null,
+                value: _endpointsData != null
+                    ? _endpointsData.values[endpoint].value
+                    : null,
               )
           ],
         ),
